3307bb6e4d194b0c4d48796d9b71fcc4a263b64b
John Gulbronson <john@benzinga.com>
2025-04-25 21:01:47 -0400

    earth key

--- src/pages/collect/[collect].tsx
+++ src/pages/collect/[collect].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
